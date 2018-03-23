2f0ebf336a3eb2e124fd939948d7280630f4a71f
Hadrien-DELAITRE <hadrien.deuxlettres@gmail.com>
2018-03-23 23:41:21 +0100

    Make eslintrc config file respect linting rules
    .eslintrc file wasn't correctly congruent with our
    linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,21 +5,18 @@ module.exports = {
     node: true
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   },
   extends: [
     "eslint:recommended",
     "plugin:vue/recommended",
     "plugin:prettier/recommended"
   ],
-  plugins: [
-    'prettier',
-    'vue'
-  ],
+  plugins: ["prettier", "vue"],
   rules: {
-    "semi": [2, "never"],
+    semi: [2, "never"],
     "no-console": "off",
     "vue/max-attributes-per-line": "off",
-    "prettier/prettier": ["error", { "semi": false }]
+    "prettier/prettier": ["error", { semi: false }]
   }
-}
+};
